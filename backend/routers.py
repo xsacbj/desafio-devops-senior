0cb43a11f9416d67d57f3aa2edae158d70d9f0a5
Sérgio Augusto <sergioaugusto.bezerra.jr@gmail.com>
2022-08-19 01:17:41 -0400

    feat: adiciona as demais rotas do sistema

--- backend/routers.py
+++ backend/routers.py
@@ -1,3 +1,10 @@
 from controllers.user import UserController
+from controllers.auth import AuthController
+from controllers.maintenance import MaintenanceController
+from controllers.service import ServiceController
+
 def setupRouters(app):
   UserController(app)
+  AuthController(app)
+  MaintenanceController(app)
+  ServiceController(app)
