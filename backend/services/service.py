92b03ba9b5ac3632f057b3798702b57aa1227191
Sérgio Augusto <sergioaugusto.bezerra.jr@gmail.com>
2022-08-21 20:36:07 -0400

    refector: generaliza função de refresh da manutenção

--- backend/services/service.py
+++ backend/services/service.py
@@ -16,8 +16,10 @@ class ServiceService:
   
   def listByMaintenanceId(self, Maintenance_id):  
     services = self.Service.query.filter_by(Maintenance_id=Maintenance_id).all()
-
-    return self.Service.serialize_list(services)
+    services = self.Service.serialize_list(services)
+    for index, service in enumerate(services):
+      del services[index]['Maintenance_id']
+    return services
 
   def findById(self, id):
     service = self.Service.query.filter_by(id=id).first()
@@ -61,50 +63,19 @@ class ServiceService:
 
     if(service is None):
       raise Exception("Service not found")
-    
-    timeDifference = 0
-    try:
-      if data['time'] is not None:
-        timeDifference = service.time - data['time']
-    except:
-      pass
 
     attributes = {'description', 'time', 'status'}
 
     for attribute in attributes:
       if(attribute in data):
         setattr(service, attribute, data[attribute])
+
     
+    
+    Maintenance_id = service.Maintenance_id
     db.session.add(service)
     db.session.commit()
-
-    maintenance = self.Maintenance.query.filter_by(id=service.Maintenance_id).first()
-    maintenance.timeEstimate = maintenance.timeEstimate - timeDifference
-    
-    services = self.Service.query.filter_by(Maintenance_id=maintenance.id).all()
-    services = self.Service.serialize_list(services)
-    
-    somePending = False
-    someInProgress = False
-    someDone = False
-    
-    for s in services:
-      if s['status'] == 'pending':
-        somePending = True
-      if s['status'] == 'inProgress':
-        someInProgress = True
-      if s['status'] == 'done':
-        someDone = True  
-    
-    if somePending and not someInProgress and not someDone:
-      maintenance.status = 'pending'
-    elif someInProgress:
-      maintenance.status = 'inProgress'
-    elif someDone and not somePending and not someInProgress:
-      maintenance.status = 'done'
-
-    db.session.add(maintenance)
-    db.session.commit()
+    self.refreshMaintenance(Maintenance_id)
 
 
     return service.serialize()
@@ -113,11 +84,51 @@ class ServiceService:
     db = self.Service.__database__
 
     service = self.Service.query.filter_by(id=id).first()
-    
+    Maintenance_id = service.Maintenance_id
+
     if(service is None):
       raise Exception("Service not found")
     
     db.session.delete(service)
     db.session.commit()
+    self.refreshMaintenance(Maintenance_id)
+
+  def refreshMaintenance(self, Maintenance_id):
+    db = self.Service.__database__
+
+    maintenance = self.Maintenance.query.filter_by(id=Maintenance_id).first()
+    
+    services = self.Service.query.filter_by(Maintenance_id=maintenance.id).all()
+    services = self.Service.serialize_list(services)
+    
+    somePending = False
+    someInProgress = False
+    someDone = False
+    time = 0
+    for s in services:
+      time = time + s['time']
+
+      if s['status'] == 'pending':
+        somePending = True
+      if s['status'] == 'inProgress':
+        someInProgress = True
+      if s['status'] == 'done':
+        someDone = True  
+
+    maintenance.timeEstimate = time
+    
+    if somePending and not someInProgress and not someDone:
+      maintenance.status = 'pending'
+    elif someInProgress or (someDone and somePending):
+      maintenance.status = 'inProgress'
+    elif someDone and not somePending and not someInProgress:
+      maintenance.status = 'done'
+
+
+
+    db.session.add(maintenance)
+    db.session.commit()
+
+
 
     
